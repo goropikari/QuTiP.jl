f860b0dfa502b7035134cc02ddb93679a23ffbd7
goropikari <goropikari@users.noreply.github.com>
2017-05-23 07:34:48 +0900

    separate identity
    To avoid conflict with Base.identity, identity operator of qutip is
    defined as qidentity

--- src/operators.jl
+++ src/operators.jl
@@ -1,2 +1,23 @@
-export jmat, spin_Jx, spin_Jy, spin_Jz, spin_Jm, spin_Jp, spin_J_set, sigmap, sigmam, sigmax, sigmay, sigmaz, destroy, create, qeye, identity, position, momentum, num, squeeze, squeezing, displace, commutator, qutrit_ops, qdiags, phase, zero_oper, enr_destroy, enr_idenitty
-const operators_class = (:jmat, :spin_Jx, :spin_Jy, :spin_Jz, :spin_Jm, :spin_Jp, :spin_J_set, :sigmap, :sigmam, :sigmax, :sigmay, :sigmaz, :destroy, :create, :qeye, :identity, :position, :momentum, :num, :squeeze, :squeezing, :displace, :commutator, :qutrit_ops, :qdiags, :phase, :zero_oper, :enr_destroy, :enr_idenitty)
+export jmat, spin_Jx, spin_Jy, spin_Jz, spin_Jm, spin_Jp, spin_J_set, sigmap, sigmam, sigmax, sigmay, sigmaz, destroy, create, qeye, position, momentum, num, squeeze, squeezing, displace, commutator, qutrit_ops, qdiags, phase, zero_oper, enr_destroy, enr_idenitty
+const operators_class = (:jmat, :spin_Jx, :spin_Jy, :spin_Jz, :spin_Jm, :spin_Jp, :spin_J_set, :sigmap, :sigmam, :sigmax, :sigmay, :sigmaz, :destroy, :create, :qeye, :position, :momentum, :num, :squeeze, :squeezing, :displace, :commutator, :qutrit_ops, :qdiags, :phase, :zero_oper, :enr_destroy, :enr_idenitty)
+
+
+export qidentity
+sf = string(:identity)
+# @eval @doc LazyHelp(qutip,$sf) function qidentity(args...; kws...)
+# 	if !haskey(qutip, $sf)
+# 		error("qutip ", version, " does not have qutip", $sf)
+# 	end
+# 	return pycall(qutip[$sf], PyAny, args...; kws...)
+# end
+
+@eval @doc LazyHelp(qutip,$sf) function qidentity(args)
+	if !haskey(qutip, $sf)
+		error("qutip ", version, " does not have qutip", $sf)
+	end
+    if  typeof(args) <: Integer
+        return pycall(qutip[$sf], PyAny, args)
+    elseif typeof(args) <: Array
+        return pycall(qutip[$sf], PyAny, [[i] for i in args])
+    end
+end
