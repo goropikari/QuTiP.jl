691ea274cd721471c6bdf15d73de3c22b094429f
goropikari <goropikari@users.noreply.github.com>
2017-06-01 07:34:47 +0900

    define some methods as function
    eliminate_states, evaluate, extract_states, overlap

--- src/attributes_methods.jl
+++ src/attributes_methods.jl
@@ -1,11 +1,11 @@
 export data, dims, shape, superrep, isherm, iscp, ishp, istp, iscptp, isket, isbra, isoper, issuper, isoperket, isoperbra
 const attributes = (:data, :dims, :shape, :type, :superrep, :isherm, :iscp, :ishp, :istp, :iscptp, :isket, :isbra, :isoper, :issuper, :isoperket, :isoperbra)
 
-export dnorm, dual_chan, tidyup, trans, transform, trunc_neg, unit
-const methods_qobj =  (:dnorm, :dual_chan, :tidyup, :trans, :transform, :trunc_neg, :unit)
+export dnorm, dual_chan, tidyup, trans, transform, trunc_neg, unit, eliminate_states, evaluate, extract_states
+const methods_qobj =  (:dnorm, :dual_chan, :tidyup, :trans, :transform, :trunc_neg, :unit, :eliminate_states, :evaluate, :extract_states)
 
-export full, norm, permute, eigenenergies, eigenstates, groundstate, matrix_element, tr, diag
-const methods = (:full, :norm, :permute, :eigenenergies, :eigenstates, :groundstate, :matrix_element, :tr, :diag)
+export full, norm, permute, eigenenergies, eigenstates, groundstate, matrix_element, tr, diag, overlap
+const methods = (:full, :norm, :permute, :eigenenergies, :eigenstates, :groundstate, :matrix_element, :tr, :diag, :overlap)
 
 # renamed
 # type 
