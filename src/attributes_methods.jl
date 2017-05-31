97f699c75bb0c895b3ba9f1f760836641f7881dc
goropikari <goropikari@users.noreply.github.com>
2017-06-01 06:09:41 +0900

    fix typo
    dual_chain -> dual_chan

--- src/attributes_methods.jl
+++ src/attributes_methods.jl
@@ -1,8 +1,8 @@
 export data, dims, shape, superrep, isherm, iscp, ishp, istp, iscptp, isket, isbra, isoper, issuper, isoperket, isoperbra
 const attributes = (:data, :dims, :shape, :type, :superrep, :isherm, :iscp, :ishp, :istp, :iscptp, :isket, :isbra, :isoper, :issuper, :isoperket, :isoperbra)
 
-export cosm, dnorm, dual_chain, eigenenergies, eigenstates, groundstate, matrix_element, sinm, tidyup, tr, trans, transform, trunc_neg, unit
-const methods =  (:cosm, :dnorm, :dual_chain, :eigenenergies, :eigenstates, :groundstate, :matrix_element, :sinm, :tidyup, :tr, :trans, :transform, :trunc_neg, :unit)
+export cosm, dnorm, dual_chan, eigenenergies, eigenstates, groundstate, matrix_element, sinm, tidyup, tr, trans, transform, trunc_neg, unit
+const methods =  (:cosm, :dnorm, :dual_chan, :eigenenergies, :eigenstates, :groundstate, :matrix_element, :sinm, :tidyup, :tr, :trans, :transform, :trunc_neg, :unit)
 
 # renamed
 # conj, expm, full, norm, permute, sqrtm, type 
