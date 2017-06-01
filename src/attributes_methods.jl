997393c7082b40cf67e1eebf4d86278933c4bbc4
goropikari <goropikari@users.noreply.github.com>
2017-06-01 12:43:29 +0900

    fix return type of dnorm

--- src/attributes_methods.jl
+++ src/attributes_methods.jl
@@ -1,11 +1,11 @@
 export data, dims, shape, superrep, isherm, iscp, ishp, istp, iscptp, isket, isbra, isoper, issuper, isoperket, isoperbra
 const attributes = (:data, :dims, :shape, :type, :superrep, :isherm, :iscp, :ishp, :istp, :iscptp, :isket, :isbra, :isoper, :issuper, :isoperket, :isoperbra)
 
-export dnorm, dual_chan, tidyup, trans, transform, trunc_neg, unit, eliminate_states, evaluate, extract_states
-const methods_qobj =  (:dnorm, :dual_chan, :tidyup, :trans, :transform, :trunc_neg, :unit, :eliminate_states, :evaluate, :extract_states)
+export dual_chan, tidyup, trans, transform, trunc_neg, unit, eliminate_states, evaluate, extract_states
+const methods_qobj =  (:dual_chan, :tidyup, :trans, :transform, :trunc_neg, :unit, :eliminate_states, :evaluate, :extract_states)
 
-export full, norm, permute, eigenenergies, eigenstates, groundstate, matrix_element, tr, diag, overlap
-const methods = (:full, :norm, :permute, :eigenenergies, :eigenstates, :groundstate, :matrix_element, :tr, :diag, :overlap)
+export full, norm, dnorm, permute, eigenenergies, eigenstates, groundstate, matrix_element, tr, diag, overlap
+const methods = (:full, :norm, :dnorm, :permute, :eigenenergies, :eigenstates, :groundstate, :matrix_element, :tr, :diag, :overlap)
 
 # renamed
 # type 
