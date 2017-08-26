54fbf78d63b585a71a0ad4cbed22a7327326473c
goropikari <goropikari@users.noreply.github.com>
2017-08-26 12:26:12 +0900

    add pretty-printing test: Vector{Quantum}

--- test/display.jl
+++ test/display.jl
@@ -5,6 +5,14 @@ catch
     false 
 end
 
+@test try
+    show(STDOUT, "text/latex", [basis(2,0), basis(2,1)])
+    true
+catch
+    false 
+end
+
+
 omega = 1.0
 es = eseries(0.5 * sigmax(), im * omega) + eseries(0.5 * sigmax(), -im * omega)
 @test try
